dce7df5c1a96f76c18ec67f088e924737e44d4ad
Konstantin <konstantin.wuestefeld@tu-dortmund.de>
2022-07-12 00:42:20 +0200

    adding functions to calculate iou between boxes

---
+++ calc_iou.py
@@ -0,0 +1,17 @@
+import sys
+
+def boxIoU(boxA, boxB):
+	# compute the area of intersection rectangle
+	dx = min(boxA[2], boxB[2]) - max(boxA[0], boxB[0])
+	dy = min(boxA[3], boxB[3]) - max(boxA[1], boxB[1])
+	if dx < 0 or dy < 0:
+		return 0.0
+	else:
+		interArea = dx * dy
+
+	# compute the area of both the prediction and ground-truth rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+	iou = interArea / float(boxAArea + boxBArea - interArea)
+	return iou
