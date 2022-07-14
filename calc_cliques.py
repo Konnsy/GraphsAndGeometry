2caac41ac93e209eb843707d5a39ced466775633
Konstantin <konstantin.wuestefeld@tu-dortmund.de>
2022-07-14 12:20:44 +0200

    calculation of cliques is now returned sorted ** adding comments

--- calc_cliques.py
+++ calc_cliques.py
@@ -1,14 +1,23 @@
 
 def calcCliques(edges, nodes):
-	""" 
-	edges: idx-edges of connected elements
-	nodes: all node idxs existent
-	result: list of node ids for each clique
+	"""
+	Calculates cliques in a graph given by a list of edges and a list of nodes.
+
+	edges: edges of connected elements, format: (idx node 1, idx node 2)
+	nodes: all node idxs existent, format: int as id
+	result: sorted list of node ids for each clique
+
+	Example usage:
+		> edges = [[1,2], [2,5], [3,0], [5,6]]
+		> nodes = [0,1,2,3,4,5,6]
+
+		> cliques = calcCliques(edges, nodes)
+		=> cliques returns [[1, 2, 5, 6], [0, 3], [4]]
 	"""
 	nodes.sort()
 	
 	# initialize connections directory
-	con = {}	
+	con = {}
 	for (a,b) in edges:
 		if a in con:
 			con[a].add(b)
@@ -26,7 +35,7 @@ def calcCliques(edges, nodes):
 				if nodes[idx1] in con and nodes[idx2] in con and nodes[idx1] in con[nodes[idx2]]:
 					con[nodes[idx2]].update(con[nodes[idx1]])
 					del con[nodes[idx1]]
-					changed = True				   
+					changed = True
 	
 	inClique = set()
 	for valList in con.values():
@@ -39,6 +48,8 @@ def calcCliques(edges, nodes):
 			cliques.append([node]+list(con[node]))
 		elif not (node in inClique):
 			cliques.append([node])
-		
-									
+
+	for idx in range(len(cliques)):
+		cliques[idx].sort()
+
 	return cliques
